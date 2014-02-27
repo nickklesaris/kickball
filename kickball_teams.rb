32a4cbefd8da6e3093179876002a22f4ba7938e0
Nick Klesaris <nickklesaris@gmail.com>
2014-02-27 14:47:37 -0500

    updated starting page

--- kickball_teams.rb
+++ kickball_teams.rb
@@ -3,29 +3,32 @@ require 'sinatra'
 require 'shotgun'
 #set :public_folder, File.dirname(__FILE__) + '/public'
 
-#get '/' do
-#end
+get '/' do
+"Change the above url to see a team\'s roster.
+ For example: http://localhost:4567/team/Simpson Slammers"
+end
+
 
 get '/team/:chosen_team' do
-	#define global data stuff
-	@team_members = []
+#define global data stuff
+@team_members = []
 
-	@teams = {}
-	CSV.foreach('lackp_starting_rosters.csv', headers: true) do |row|
-		full_name = row['first_name'] + ' ' + row['last_name']
-		@teams[full_name] = row.to_hash
-	end
+@teams = {}
+CSV.foreach('lackp_starting_rosters.csv', headers: true) do |row|
+full_name = row['first_name'] + ' ' + row['last_name']
+@teams[full_name] = row.to_hash
+end
 
-	#puts 'Choose your team.'
-	#@chosen_team = gets.chomp
-	@chosen_team = params[:chosen_team]
-	#@chosen_team = 'Flinestone Fire'
+#puts 'Choose your team.'
+#@chosen_team = gets.chomp
+@chosen_team = params[:chosen_team]
+#@chosen_team = 'Flinestone Fire'
 
-	@teams.each do |member|
-		if member[1]['team'] == @chosen_team
-			@team_members << member[1]['first_name']+' '+member[1]['last_name']+': '+member[1]['position']
-		end
-	end
+@teams.each do |member|
+if member[1]['team'] == @chosen_team
+@team_members << member[1]['first_name']+' '+member[1]['last_name']+': '+member[1]['position']
+end
+end
 erb:index
 end
 # ---recently commented out---
